c8da347e96167df0bef7d152ed769c74dea2e67a
JoseDev16 <42664769+JoseDev16@users.noreply.github.com>
2020-06-28 01:47:17 -0600

    loginbasico

--- notasCrud/routes/web.php
+++ notasCrud/routes/web.php
@@ -11,8 +11,12 @@
 |
 */
 Route::view('/', 'welcome');
+Route::view('/home', 'welcome');
 Route::get('/notas', 'NotaController@index')->name('home');
 Route::post('/new', 'NotaController@store')->name('store');
 Route::get('/editar/{id}', 'NotaController@edit')->name('editar');
 Route::put('/update/{id}', 'NotaController@update')->name('update');
 Route::delete('/delete/{id}', 'NotaController@destroy')->name('eliminar');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
